463d467957a9eab33d69ed0f998b679ad4f7713b
warmb2009 <2462787827@qq.com>
2017-11-30 22:26:30 +0800

    修改

--- tbinfo.py
+++ tbinfo.py
@@ -1,78 +1,82 @@
 # -*- coding:utf-8 -*-
 
 import re
-from tbk.TbkItemConvert import *
-from tbk.TbkItemConvert import TbkItemConvertCoupon
+from tbk.TbkSeniorAPI import *
 from tbk.loggingset import create_logger
 import requests
+import json
 
 Loger = create_logger()
-postURL = 'http://127.0.0.1:8080/ajax/tbinfoadd/'
+postURL = 'http://127.0.0.1:8080/tkapis/'
+tbk = tbk()
 
-def postInfo(url, dic):
+def postInfo(url, dic, itemID):
+#    datas = json.dumps(dic)
+    url = url #+ 'item_id=' + itemID + '/'
     r = requests.post(url, data = dic)
     print(r)
+    Loger.info('post info')
     
-#正则表达式获取券链接和商品链接
-def get_url(text):
+# 正则表达式获取券链接和商品链接
+def getCommodityInfo(text, pid):
+    # 处理获取的信息
     matchObj = re.findall( r'(.*)领券(.*)抢购(.*https?://[ -~]+.*id=\d{11,12})(.*)', text)
     print(matchObj)
-    if matchObj:
-        return matchObj
-    else:
+    if not matchObj:
         matchObj = re.findall( r'(.*)领券(.*)抢购(.*https?://[ -~]+[A-Za_z])(.*)', text)
-        if matchObj:
-            return matchObj
-        else:
+        if not matchObj:
             return False
+
+    link = matchObj[0]
+
+    title = link[0].strip()
+    price = ''
+    couponURL = link[1].strip()[1:]#优惠券链接
+    commodityURL = link[2].strip()[1:]#商品链接
+    content = link[3].strip()
+        
+    # 测试商品链接并转换
+    commodityURL = testCommodity(commodityURL)
+        
+    # 获取推广链接及商品图片 模拟登陆
+    #    promotion_text, img_src = get(commodity)
+    itemID = tbk.getItemID(commodityURL)
+    
+    # 将商品加入定向计划 调用全局变量, 传入商品url
+    my_coupon_url = tbk.conver_url(couponURL, itemID, pid)
+    # 通过api获取商品信息
+    commodityInfo = tbk.getCommodityInfo(itemID)
+    img_src = commodityInfo['pic']
+    item_url = commodityInfo['item_url']
+    location = commodityInfo['location']
+    
+    # 淘口令
+    model = tbk.getTpwd(title, my_coupon_url)
+
+    dic = {}
+    dic['item_id'] = itemID
+    dic['item_title'] = title
+    # dic['item_price'] = price
+    dic['item_coupon'] = my_coupon_url
+    dic['item_url'] = item_url
+    dic['item_content'] = content
+    dic['item_image'] = img_src
+    dic['item_model'] = model
+    dic['item_location'] = location
+    
+    printdic(dic)
+    postInfo(postURL, dic, itemID)
+    return dic
     
 def printdic(dic):
     for key in dic.keys():
         Loger.info((key, '-',dic[key]))
 
+# 测试url是否需要跳转,获取跳转后链接 
 def testCommodity(url):
     Loger.info(url)
     f_url = url[0:26]
     Loger.info(f_url)
     if f_url == 'https://s.click.taobao.com':
-        url = decodeURL(url)
+        url = tbk.getTrueURL(url)
     return url
-
-#将从QQ群获取的内容转换为券链接和商品链接
-def convert(text, pid):
-    links = get_url(text)
-    if links is not False:
-        #获取简单数据
-        link = links[0]
-        
-        title = link[0].strip()
-        price = ''
-        coupon = link[1].strip()[1:]#优惠券
-        commodity = link[2].strip()[1:]#商品
-        content = link[3].strip()
-
-        commodity = testCommodity(commodity)
-        
-        #获取推广链接及商品图片
-        promotion_text, img_src = get(commodity)
-        
-        #将商品加入定向计划 调用全局变量, 传入商品url
-        TICC = TbkItemConvertCoupon()
-        my_coupon_url, itemID = TICC.conver_url(coupon, commodity, pid)
-        
-        dic = {}
-        dic['itemid'] = itemID
-        dic['title'] = title
-        dic['price'] = price
-        dic['coupon'] = my_coupon_url
-        dic['commodity'] = promotion_text
-        dic['content'] = content
-        dic['imageurl'] = img_src
-
-        printdic(dic)
-        postInfo(postURL, dic)
-        return dic
-    else:
-        return False
-
-    
